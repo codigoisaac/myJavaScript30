e87b57ad73e917ddae833468a6a5681af561d5bd
Isaac Muniz <isaacdmuniz@yahoo.com>
2020-06-26 16:39:51 -0300

    Start adding controls.
    Added Line Width and Reset Point.
    Adding color next.

--- 08 - Fun with HTML5 Canvas/main.js
+++ 08 - Fun with HTML5 Canvas/main.js
@@ -1,9 +1,11 @@
 const canvas = document.querySelector("#draw");
-const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext("2d");
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
+
 // enable to add shadow
 // ctx.globalCompositeOperation = "multiply";
 
@@ -11,7 +13,19 @@ let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let hue = 0;
-let direction = true;
+let direction = false;
+let isResetPoint = true;
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  // reset point
+  if (isResetPoint) {
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+  }
+});
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
 
 function draw(e) {
   if (!isDrawing) return;
@@ -31,19 +45,49 @@ function draw(e) {
   if (hue >= 360) {
     hue = 0;
   }
-  // change line width
-  // ctx.lineWidth = hue;
-  if (ctx.lineWidth >= 200 || ctx.lineWidth <= 1) {
-    direction = !direction; // flip direction
+
+  // change Line Width
+  if (isAutoLW) {
+    if (ctx.lineWidth >= maxLW || ctx.lineWidth <= 1) {
+      direction = !direction;
+    }
+    direction ? ctx.lineWidth++ : ctx.lineWidth--;
+    lineWInput.value = ctx.lineWidth;
   }
-  direction ? ctx.lineWidth++ : ctx.lineWidth--;
 }
 
-canvas.addEventListener("mousemove", draw);
-canvas.addEventListener("mousedown", (e) => {
-  isDrawing = true;
-  // reset point (remove to see bug)
-  [lastX, lastY] = [e.offsetX, e.offsetY];
-});
-canvas.addEventListener("mouseup", () => (isDrawing = false));
-canvas.addEventListener("mouseout", () => (isDrawing = false));
+// Add Controls
+
+let isAutoLW = true;
+let maxLW = 200;
+
+// Line Width
+const lineWInput = document.querySelector("input[id=lineWidth]");
+lineWInput.addEventListener("change", changeLineW);
+
+function changeLineW() {
+  ctx.lineWidth = lineWInput.value;
+  if (lineWInput.value > maxLWInput.value) {
+    maxLWInput.value = lineWInput.value;
+  }
+}
+
+// Auto Line Width
+const autoLWInput = document.querySelector("input[id=autoLineWidth]");
+autoLWInput.addEventListener("change", toggleAutoLW);
+
+function toggleAutoLW() {
+  isAutoLW = autoLWInput.checked;
+  maxLWInput.disabled = isAutoLW ? false : true;
+}
+
+// Max Line Width
+const maxLWInput = document.querySelector("input[id=maxLineWidth]");
+maxLWInput.addEventListener("change", () => (maxLW = maxLWInput.value));
+
+// Reset Point
+const resetPointInput = document.querySelector("input[id=resetPoint");
+resetPointInput.addEventListener(
+  "change",
+  () => (isResetPoint = !isResetPoint)
+);
